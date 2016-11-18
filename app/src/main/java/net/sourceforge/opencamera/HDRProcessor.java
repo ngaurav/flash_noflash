cc16e06eae783f8cbde5982fe312be638ff76b19
Nishant Gaurav <ng@Nishants-MacBook-Pro.local>
2016-11-18 20:33:51 +0530

    billateral

--- app/src/main/java/net/sourceforge/opencamera/HDRProcessor.java
+++ app/src/main/java/net/sourceforge/opencamera/HDRProcessor.java
@@ -751,57 +751,49 @@ public class HDRProcessor {
 	private void processHDRAverage(List<Bitmap> bitmaps) {
 		if( MyDebug.LOG )
 			Log.d(TAG, "processHDRAverage");
+        
+        if( Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT ) {
+            if( MyDebug.LOG )
+                Log.e(TAG, "HDR requires at least Android 4.4");
+            throw new RuntimeException();
+        }
+        
     	long time_s = System.currentTimeMillis();
-		
-		Bitmap bm = bitmaps.get(0);
-		int n_bitmaps = bitmaps.size();
-		int [] total_r = new int[bm.getWidth()*bm.getHeight()];
-		int [] total_g = new int[bm.getWidth()*bm.getHeight()];
-		int [] total_b = new int[bm.getWidth()*bm.getHeight()];
-		for(int i=0;i<bm.getWidth()*bm.getHeight();i++) {
-			total_r[i] = 0;
-			total_g[i] = 0;
-			total_b[i] = 0;
-		}
-		//int [] buffer = new int[bm.getWidth()*bm.getHeight()];
-		int [] buffer = new int[bm.getWidth()];
-		for(int i=0;i<n_bitmaps;i++) {
-			//bitmaps.get(i).getPixels(buffer, 0, bm.getWidth(), 0, 0, bm.getWidth(), bm.getHeight());
-			for(int y=0,c=0;y<bm.getHeight();y++) {
-				if( MyDebug.LOG ) {
-					if( y % 100 == 0 )
-						Log.d(TAG, "process " + i + ": " + y + " / " + bm.getHeight());
-				}
-				bitmaps.get(i).getPixels(buffer, 0, bm.getWidth(), 0, y, bm.getWidth(), 1);
-				for(int x=0;x<bm.getWidth();x++,c++) {
-					//int this_col = buffer[c];
-					int this_col = buffer[x];
-					total_r[c] += this_col & 0xFF0000;
-					total_g[c] += this_col & 0xFF00;
-					total_b[c] += this_col & 0xFF;
-				}
-			}
-		}
-		if( MyDebug.LOG )
-			Log.d(TAG, "time before write: " + (System.currentTimeMillis() - time_s));
-		// write:
-		for(int y=0,c=0;y<bm.getHeight();y++) {
-			if( MyDebug.LOG ) {
-				if( y % 100 == 0 )
-					Log.d(TAG, "write: " + y + " / " + bm.getHeight());
-			}
-			for(int x=0;x<bm.getWidth();x++,c++) {
-				total_r[c] /= n_bitmaps;
-				total_g[c] /= n_bitmaps;
-				total_b[c] /= n_bitmaps;
-				//int col = Color.rgb(total_r[c] >> 16, total_g[c] >> 8, total_b[c]);
-				int col = (total_r[c] & 0xFF0000) | (total_g[c] & 0xFF00) | total_b[c];
-				buffer[x] = col;
-			}
-			bm.setPixels(buffer, 0, bm.getWidth(), 0, y, bm.getWidth(), 1);
-		}
+        
+        int n_bitmaps = bitmaps.size();
+        Bitmap bm = bitmaps.get(0);
+        
+        if( rs == null ) {
+            this.rs = RenderScript.create(context);
+            if( MyDebug.LOG )
+                Log.d(TAG, "create renderscript object");
+            if( MyDebug.LOG )
+                Log.d(TAG, "time after creating renderscript: " + (System.currentTimeMillis() - time_s));
+        }
+        
+        // create allocations
+        Allocation [] allocations = new Allocation[n_bitmaps];
+        for(int i=0;i<n_bitmaps;i++) {
+            allocations[i] = Allocation.createFromBitmap(rs, bitmaps.get(i));
+        }
+        
+        // create RenderScript
+        ScriptC_joint_bilateral processHDRScript = new ScriptC_joint_bilateral(rs);
 
-		if( MyDebug.LOG )
-			Log.d(TAG, "time for processHDRAverage: " + (System.currentTimeMillis() - time_s));
+        // set allocations
+        processHDRScript.set_bitmap1(allocations[0]);
+		processHDRScript.set_floatBuffer(allocations[1]);
+		processHDRScript.set_sImageWidth(bm.getWidth());
+		processHDRScript.set_sImageHeight(bm.getHeight());
+        
+        if( MyDebug.LOG )
+            Log.d(TAG, "call processHDRScript");
+        processHDRScript.forEach_bilateral(allocations[0], allocations[0]);
+        if( MyDebug.LOG )
+            Log.d(TAG, "time after processHDRScript: " + (System.currentTimeMillis() - time_s));
+
+        allocations[0].copyTo(bm);
+        if( MyDebug.LOG )
+            Log.d(TAG, "time after copying to bitmap: " + (System.currentTimeMillis() - time_s));
 	}
 }
---
+++ app/src/main/rs/joint_bilateral.rs
@@ -0,0 +1,101 @@
+#pragma version(1)
+#pragma rs java_package_name(net.sourceforge.opencamera)
+#pragma rs_fp_relaxed
+
+// size of Gauss matrix (bilateral filter - pixel position distance)
+#define GM_SIZE 5
+// size of Gauss matrix (noice reduction)
+#define GSM_SIZE 3
+// sigma of gauss function (bilateral filter - pixel intensity distance)
+#define GAUSS_SIGMA 0.1f // for intensity values in float3 image space
+
+// pre-evaluated multiplier for transforming byte RGBa values [0;255] to float RGB [0;1]
+const float normU8 = 1.f/255.f;
+// radiuses of matrices used
+const uint gmBorder = (GM_SIZE - 1) / 2;
+const uint gsmBorder = (GSM_SIZE - 1) / 2;
+
+// filter output resolution parameters
+uint sImageWidth;
+uint sImageHeight;
+
+// pre-evaluated multiplier in gauss function exponent (bilateral filter)
+// they are evaluated in "init" function
+float gaussExpMult;
+float gaussMult;
+
+// Gauss matrix used on camera input image to reduce noice by smoothing
+static float gaussSmoothMatrix[GSM_SIZE][GSM_SIZE] = {
+    {0.109021f,	0.112141f,	0.109021f},
+    {0.112141f,	0.11535f,	0.112141f},
+    {0.109021f,	0.112141f,	0.109021f}
+};
+
+// Gauss matrix used in bilateral filter (pixel position distance)
+static float gaussMatrix[GM_SIZE][GM_SIZE] = {
+    {0.010534f,	0.02453f,	0.032508f,	0.02453f,	0.010534f},
+    {0.02453f,	0.05712f,	0.075698f,	0.05712f,	0.02453f},
+    {0.032508f,	0.075698f,	0.100318f,	0.075698f,	0.032508f},
+    {0.02453f,	0.05712f,	0.075698f,	0.05712f,	0.02453f},
+    {0.010534f,	0.02453f,	0.032508f,	0.02453f,	0.010534f}
+};
+
+// buffer containing input image buffer for kernels
+rs_allocation bitmap1;
+rs_allocation floatBuffer;
+
+inline static float3 zero_float3() {
+    float3 c;
+    c.r = 0.f;
+    c.g = 0.f;
+    c.b = 0.f;
+    return c;
+}
+
+// Evaluates value of a gauss function (normal distribution with pre-evaluated exponent multiplier)
+inline static float gauss(float x) {
+    return gaussMult * native_exp(gaussExpMult * x * x);
+}
+
+// Evaluates intensity of an RGB pixel = lenght of the vector
+inline static float intensity(float3 rgb) {
+    return fast_length(rgb);
+}
+
+// Evaluates intensity difference between two RGB pixels
+// Applies logarithm and division to reduce "halo" effect around edges
+inline static float intensityDifference(float3 x, float3 y) {
+    return native_log((intensity(x) + 0.01f) / (intensity(y) + 0.01f));
+}
+
+void init() {
+    gaussExpMult = - 1.f / (2.f * GAUSS_SIGMA * GAUSS_SIGMA);
+    gaussMult = 1.f / sqrt(2.f * M_PI * GAUSS_SIGMA * GAUSS_SIGMA);
+}
+
+
+// Applies bilateral filter on an image
+float3 __attribute__((kernel)) applyBilateralFilter_float3(uint32_t x, uint32_t y) {
+    float3 result = zero_float3();
+    float W = 0.f;
+
+    for (uint i = 0; i < GM_SIZE; ++i) {
+        for (uint j = 0; j < GM_SIZE; ++j) {
+            float gm = gaussMatrix[i][j];
+            float3 noflash_pixel = rsGetElementAt_float3(bitmap1, clamp(x + i - gmBorder, 0u, sImageWidth - 1u), clamp(y + j - gmBorder, 0u, sImageHeight - 1u));
+            float3 pixel = rsGetElementAt_float3(floatBuffer, clamp(x + i - gmBorder, 0u, sImageWidth - 1u), clamp(y + j - gmBorder, 0u, sImageHeight - 1u));
+            float3 origin = rsGetElementAt_float3(floatBuffer, x, y);
+            float ev = gm * gauss(intensityDifference(pixel, origin));
+            W += ev;
+            result += ev * noflash_pixel;
+        }
+    }
+
+    return result / W;
+}
+
+
+// Applies bilateral filter and transforms resulting float RGB pixel to byte RGBa pixel
+uchar4 __attribute__((kernel)) bilateral(uchar4 in, uint32_t x, uint32_t y) {
+    return rsPackColorTo8888(applyBilateralFilter_float3(x, y));
+}
